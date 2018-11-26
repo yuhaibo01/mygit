2ce622ca6ef20068cbf922dbd8f209430daed40d
yuhaibo01 <haiboyu2004@163.com>
2018-11-26 14:18:14 +0800

    发布项目

---
+++ Cloud-Maven/src/main/java/baoneng/Cloud_Maven/App.java
@@ -0,0 +1,13 @@
+package baoneng.Cloud_Maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
